6a38564bd12b4699063620a584b171af23e44ed0
willslima <williansantos38@gmail.com>
2023-12-19 17:38:52 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Text, SafeAreaView } from 'react-native'
+import Home from './src/telas/Home'
+
+export default function App(){
+  return <SafeAreaView>
+  <Home />
+  </SafeAreaView>
+}
---
+++ src/telas/Home/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Topo from './componentes/Topo'
+
+import topo from '../../mocks/topo'
+
+export default function Home(){
+    return <Topo {...topo}/>
+}
